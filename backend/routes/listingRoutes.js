7ccb5c211dd494c71411d8938bae5ce29d204da0
dalalanshraj <dalalanshraj@gmail.com>
2026-06-13 00:52:35 +0530

    ok

--- backend/routes/listingRoutes.js
+++ backend/routes/listingRoutes.js
@@ -92,54 +92,6 @@ router.get("/public", async (req, res) => {
 router.post("/", createListing);
 
 router.get("/", isAuth, isAdmin, getAllListings);
-router.get("/:id", getListingById); 
-router.delete("/:id", deleteListing);
- 
-
-// tab-wise save
-router.put("/:id/property", updateProperty);
-router.put("/:id/description", updateDescription);
-router.put("/:id/amenities", updateAmenities);
-router.put("/:id/activities", updateActivities);
-router.put(
-  "/:id/photos",
-  upload.array("photos", 60),
-  updatePhotos
-);
-router.delete("/:id/photos/:filename", deletePhoto);
-router.put(
-  "/:id/photos/reorder",
-  reorderPhotos
-);
-router.put("/:id/video", updateVideo);
-router.put("/:id/rates", updateRates);
-router.put("/:id/rates/delete", deleteRate);
-router.put("/:id/rates/edit", editRate);
-router.put("/:id/location", updateLocation);
-
-
-router.put("/:id/publish", publishListing);
-
-//! Reviews Route 
-router.post("/:id/reviews", addReview);
-
-router.put("/:id/reviews/:reviewId/publish", publishReview);
-router.put("/:id/reviews/:reviewId/reply", replyReview);
-router.delete("/:id/reviews/:reviewId", deleteReview);
-
-
-//! EXTRA FEES
-router.put("/:id/extra-fees", addExtraFee);
-router.put("/:id/extra-fees/edit", editExtraFee);
-router.put("/:id/extra-fees/delete", deleteExtraFee);
-
-router.put(
-  "/:id/toggle-status",
-  isAuth,
-  isAdmin,
-  toggleListingStatus
-);
-
 //! community
 router.get("/communities/all", async (req, res) => {
   try {
@@ -204,6 +156,55 @@ router.get("/community/:community", async (req, res) => {
 
   }
 });
+router.get("/:id", getListingById); 
+router.delete("/:id", deleteListing);
+ 
+
+// tab-wise save
+router.put("/:id/property", updateProperty);
+router.put("/:id/description", updateDescription);
+router.put("/:id/amenities", updateAmenities);
+router.put("/:id/activities", updateActivities);
+router.put(
+  "/:id/photos",
+  upload.array("photos", 60),
+  updatePhotos
+);
+router.delete("/:id/photos/:filename", deletePhoto);
+router.put(
+  "/:id/photos/reorder",
+  reorderPhotos
+);
+router.put("/:id/video", updateVideo);
+router.put("/:id/rates", updateRates);
+router.put("/:id/rates/delete", deleteRate);
+router.put("/:id/rates/edit", editRate);
+router.put("/:id/location", updateLocation);
+
+
+router.put("/:id/publish", publishListing);
+
+//! Reviews Route 
+router.post("/:id/reviews", addReview);
+
+router.put("/:id/reviews/:reviewId/publish", publishReview);
+router.put("/:id/reviews/:reviewId/reply", replyReview);
+router.delete("/:id/reviews/:reviewId", deleteReview);
+
+
+//! EXTRA FEES
+router.put("/:id/extra-fees", addExtraFee);
+router.put("/:id/extra-fees/edit", editExtraFee);
+router.put("/:id/extra-fees/delete", deleteExtraFee);
+
+router.put(
+  "/:id/toggle-status",
+  isAuth,
+  isAdmin,
+  toggleListingStatus
+);
+
+
 
 export default router;
   
