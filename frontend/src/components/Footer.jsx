26884a1991bbf0c5782c5ac5997fb95e8242e56f
dalalanshraj <dalalanshraj@gmail.com>
2026-06-13 04:00:20 +0530

    ok

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -61,7 +61,7 @@ const Footer = () => {
               <li><Link to={"/about"} className="hover:text-[#3c8a8c] transition-colors">About</Link></li>
               <li><Link to={"/contact"} className="hover:text-[#3c8a8c] transition-colors">Contact Us</Link></li>
               <li><Link to={"/activities"} className="hover:text-[#3c8a8c] transition-colors"> Activities</Link></li>
-              <li><Link to={"/property-management"} className="hover:text-[#3c8a8c] transition-colors"> Property Management</Link></li>
+              {/* <li><Link to={"/property-management"} className="hover:text-[#3c8a8c] transition-colors"> Property Management</Link></li> */}
             </ul>
           </div>
 
