8926e4661b65820bd691ef995aae6718015283ff
dalalanshraj <dalalanshraj@gmail.com>
2026-08-28 00:01:42 +0530

    ok

--- frontend/src/components/ImageGallery.jsx
+++ frontend/src/components/ImageGallery.jsx
@@ -68,11 +68,11 @@ const ImageGallery = ({ photos = [] }) => {
   return (
     <section className="w-full">
       {/* Main Image */}
-      <div className="relative h-[400px] md:h-[500px] overflow-hidden rounded-xl shadow-lg">
+      <div className="relative h-[400px] md:h-[700px] overflow-hidden rounded-xl shadow-lg">
         <img
           src={images[currentImageIndex]}
           alt="Property"
-          className="w-full h-full object-cover"
+          className="w-full h-full  "
         />
 
         <button
